7e5d6aa22e26c2db32908c385f1f9fec2167d99d
Wil Cooley <wcooley@nakedape.cc>
2006-08-02 00:22:46 +0000

    Create external functions for passwd alloc, copy, destroy in exfiles-util.c

--- src/nss_exfiles.c
+++ src/nss_exfiles.c
@@ -2,16 +2,20 @@
 #include <string.h>
 #include <stdio.h>
 
-#include "nss_exfiles.h"
+#include "exfiles-util.h"
 #include "strsplit.h"
+#include "nss_exfiles.h"
 
 EXFILE_FILE(passwd)
 
-static FILE * ex_passwd_f;
+static FILE *ex_passwd_f;
 
-
+/*
+ * Open the passwd file for reading
+ */
 enum nss_status 
-_nss_exfiles_setpwent(void) {
+_nss_exfiles_setpwent(void) 
+{
 
     enum nss_status status = NSS_STATUS_SUCCESS;
     char err_msg[128] = "_nss_exfiles_setpwent: Failed to open the"
@@ -29,6 +33,9 @@ _nss_exfiles_setpwent(void) {
     return status;
 }
 
+/*
+ * Close the passwd file
+ */
 enum nss_status 
 _nss_exfiles_endpwent(void) {
 
@@ -48,7 +55,7 @@ _nss_exfiles_getpwent_r(struct passwd *pwbuf,
                         int *errnop)
 {
     enum nss_status status = NSS_STATUS_SUCCESS;
-    char ** pw_entry = NULL;
+    char **pw_entry = NULL;
     char pwline[MAX_CANON];
     int llength = 0;
     int scan_len;
@@ -58,27 +65,20 @@ _nss_exfiles_getpwent_r(struct passwd *pwbuf,
         return NSS_STATUS_NOTFOUND;
     }
 
-    /*fprintf(stderr, "getpwent called\n", __LINE__);*/
-
-    if (NULL == fgets(pwline, MAX_CANON, ex_passwd_f))
+    if (NULL == fgets(pwline, MAX_CANON, ex_passwd_f)) {
         return NSS_STATUS_NOTFOUND;
+    }
     
     llength = strlen(pwline);
 
     pwline[llength-1] = '\0';
-/*    fprintf(stderr, "Read line '%s'\n", pwline); */
 
     pw_entry = strsplit(pwline, ':');
 
     if (NULL == pw_entry)
         return NSS_STATUS_NOTFOUND;
 
-    /* Allocate storage */
-    pwbuf->pw_name      = malloc(sizeof(char) * (strlen(pw_entry[0])+1));
-    pwbuf->pw_passwd    = malloc(sizeof(char) * (strlen(pw_entry[1])+1));
-    pwbuf->pw_gecos     = malloc(sizeof(char) * (strlen(pw_entry[4])+1));
-    pwbuf->pw_dir       = malloc(sizeof(char) * (strlen(pw_entry[5])+1));
-    pwbuf->pw_shell     = malloc(sizeof(char) * (strlen(pw_entry[6])+1));
+    exfiles_alloc_passwd_from_pw_entry(pwbuf, pw_entry);
 
     if (   NULL == pwbuf->pw_name
         || NULL == pwbuf->pw_passwd
@@ -86,46 +86,17 @@ _nss_exfiles_getpwent_r(struct passwd *pwbuf,
         || NULL == pwbuf->pw_dir
         || NULL == pwbuf->pw_shell
     ) {
-        if ( NULL != pwbuf->pw_shell )
-            free(pwbuf->pw_shell);
-        if ( NULL != pwbuf->pw_dir )
-            free(pwbuf->pw_dir);
-        if ( NULL != pwbuf->pw_gecos )
-            free(pwbuf->pw_gecos);
-        if ( NULL != pwbuf->pw_shell )
-            free(pwbuf->pw_shell);
-        if ( NULL != pwbuf->pw_dir )
-            free(pwbuf->pw_dir);
-        if ( NULL != pwbuf->pw_gecos )
-            free(pwbuf->pw_gecos);
-        if ( NULL != pwbuf->pw_passwd )
-            free(pwbuf->pw_passwd);
-        if ( NULL != pwbuf->pw_name )
-            free(pwbuf->pw_name);
-        /* pwbuf is not free()d since it's passed in */
-
+        /* pwbuf is not free()d since it's passed in by the user */
+        exfiles_passwd_destroy(pwbuf);
         perror("Error allocating memory for member of struct passwd");
         return NSS_STATUS_NOTFOUND;
     }
 
-    /* Copy username */
-    strncpy(pwbuf->pw_name, pw_entry[0], strlen(pw_entry[0])+1);
-
-    /* Copy password */
-    strncpy(pwbuf->pw_passwd, pw_entry[1], strlen(pw_entry[1])+1);
-
-    /* Copy and convert to int UID and GID */
-    pwbuf->pw_uid = (uid_t)atoi(pw_entry[2]);
-    pwbuf->pw_gid = (gid_t)atoi(pw_entry[3]);
-
-    /* Copy GECOS field */
-    strncpy(pwbuf->pw_gecos, pw_entry[4], strlen(pw_entry[4])+1);
-
-    /* Copy home directory */
-    strncpy(pwbuf->pw_dir, pw_entry[5], strlen(pw_entry[5])+1);
-
-    /* Copy Shell */
-    strncpy(pwbuf->pw_shell, pw_entry[6], strlen(pw_entry[6])+1);
+    if (NULL == exfiles_copy_passwd_from_pw_entry(pwbuf, pw_entry)) {
+        exfiles_passwd_destroy(pwbuf);
+        perror("Failure copying from pw_entry to pwbuf");
+        return NSS_STATUS_NOTFOUND;
+    }
 
     return status;
 }
@@ -169,8 +140,6 @@ _nss_exfiles_getpwnam_r(const char *pwnam,
 
     pwnam_length = strlen(pwnam);
 
-/*    fprintf(stderr, "_nss_exfiles_getpwnam_r called\n"); */
-
     _nss_exfiles_setpwent();
     while (NSS_STATUS_SUCCESS == _nss_exfiles_getpwent_r(pwbuf, NULL, 0, NULL)){
         /* Compare requested username with current entry 
@@ -186,7 +155,6 @@ _nss_exfiles_getpwnam_r(const char *pwnam,
         }
     }
     _nss_exfiles_endpwent();
-    fprintf(stderr, "_nss_exfiles_getpwnam_r exiting with status %d\n", status);
     return status;
 }
 
