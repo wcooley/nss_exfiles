29ef756c4b95c775fee7abede2bcc4916ab27838
Wil Cooley <wcooley@nakedape.cc>
2007-02-05 06:35:30 +0000

    Check status of setpwent; added getpwbyuid; added tracing points; do cleanup after reporting errors, in case there are errors in the cleanup, to give a better idea of where failure has occurred; move setpwent file opening into exfiles_open_file

--- src/nss_exfiles.c
+++ src/nss_exfiles.c
@@ -2,9 +2,9 @@
 #include <string.h>
 #include <stdio.h>
 
+#include "nss_exfiles.h"
 #include "exfiles-util.h"
 #include "strsplit.h"
-#include "nss_exfiles.h"
 
 EXFILE_FILE(passwd)
 
@@ -16,21 +16,8 @@ static FILE *ex_passwd_f;
 enum nss_status 
 _nss_exfiles_setpwent(void) 
 {
-
-    enum nss_status status = NSS_STATUS_SUCCESS;
-    char err_msg[128] = "_nss_exfiles_setpwent: Failed to open the"
-                        " password file '%s'";
-
-    /*fprintf(stderr, "setpwent called\n");*/
-    ex_passwd_f = fopen(ex_passwd, "r");
-
-    if (NULL == ex_passwd_f) {
-        sprintf(err_msg, ex_passwd);
-        perror(err_msg);
-        status = NSS_STATUS_UNAVAIL;
-    }
-
-    return status;
+    exfiles_trace_msg("Entering _nss_exfiles_setpwent");
+    return exfiles_open_file(ex_passwd, &ex_passwd_f);
 }
 
 /*
@@ -41,8 +28,10 @@ _nss_exfiles_endpwent(void) {
 
     enum nss_status status = NSS_STATUS_SUCCESS;
 
-    if (NULL != ex_passwd_f)
+    if (NULL != ex_passwd_f) {
         fclose(ex_passwd_f);
+        ex_passwd_f = NULL;
+    }
 
     return status;
 }
@@ -55,13 +44,20 @@ _nss_exfiles_getpwent_r(struct passwd *pwbuf,
                         int *errnop)
 {
     enum nss_status status = NSS_STATUS_SUCCESS;
-    char **pw_entry = NULL;
-    char pwline[MAX_CANON];
-    int llength = 0;
-    int scan_len;
+    char **pw_entry = NULL;     /* array of strings for each passwd field */
+    char pwline[MAX_CANON];     /* buffer for line from password file */
+    int llength = 0;            /* actual string length of pwline */
+    int scan_len;               /* FIXME Not used?  Remove */
+
+    exfiles_trace_msg("Entering _nss_exfiles_getpwent_r");
 
     if (NULL == pwbuf) {
         fprintf(stderr, "pwbuf is NULL\n");
+        return NSS_STATUS_NOTFOUND;
+    }
+
+    if (NULL == ex_passwd_f) {
+        fprintf(stderr, "ex_passwd_f is NULL\n");
         return NSS_STATUS_NOTFOUND;
     }
 
@@ -73,30 +69,30 @@ _nss_exfiles_getpwent_r(struct passwd *pwbuf,
 
     pwline[llength-1] = '\0';
 
+    exfiles_trace_msg("Splitting pwline");
     pw_entry = strsplit(pwline, ':');
 
     if (NULL == pw_entry)
         return NSS_STATUS_NOTFOUND;
 
-    exfiles_alloc_passwd_from_pw_entry(pwbuf, pw_entry);
+    exfiles_trace_msg("Allocating memory for passwd entry");
 
-    if (   NULL == pwbuf->pw_name
-        || NULL == pwbuf->pw_passwd
-        || NULL == pwbuf->pw_gecos
-        || NULL == pwbuf->pw_dir
-        || NULL == pwbuf->pw_shell
-    ) {
+    if ( 0 != exfiles_alloc_passwd_from_pw_entry(pwbuf, pw_entry) ) {
         /* pwbuf is not free()d since it's passed in by the user */
-        exfiles_passwd_destroy(pwbuf);
         perror("Error allocating memory for member of struct passwd");
+        exfiles_passwd_destroy(pwbuf);
         return NSS_STATUS_NOTFOUND;
     }
 
-    if (NULL == exfiles_copy_passwd_from_pw_entry(pwbuf, pw_entry)) {
-        exfiles_passwd_destroy(pwbuf);
+    exfiles_trace_msg("Copying pw_entry to passwd");
+
+    if ( 0 != exfiles_copy_passwd_from_pw_entry(pwbuf, pw_entry) ) {
         perror("Failure copying from pw_entry to pwbuf");
+        exfiles_passwd_destroy(pwbuf);
         return NSS_STATUS_NOTFOUND;
     }
+
+    exfiles_trace_msg("Returning from _nss_exfiles_getpwent_r");
 
     return status;
 }
@@ -111,19 +107,38 @@ _nss_exfiles_getpwuid_r(uid_t uid,
     /* Default status */
     enum nss_status status = NSS_STATUS_NOTFOUND;
 
-    /* fprintf(stderr, "_nss_exfiles_getpwuid_r called\n"); */
+    exfiles_trace_msg("Entering _nss_exfiles_getpwuid_r");
 
-    _nss_exfiles_setpwent(); 
-    while (NSS_STATUS_SUCCESS 
-            == _nss_exfiles_getpwent_r(pwbuf, NULL, 0, NULL)){
+    if (NSS_STATUS_SUCCESS == _nss_exfiles_setpwent()) {
+        while (NSS_STATUS_SUCCESS 
+                == _nss_exfiles_getpwent_r(pwbuf, buffer, buflen, errnop)){
 
-        if (uid == pwbuf->pw_uid) {
-            status = NSS_STATUS_SUCCESS;
-            break;
+            if (uid == pwbuf->pw_uid) {
+                status = NSS_STATUS_SUCCESS;
+                break;
+            }
         }
     }
     _nss_exfiles_endpwent();
     return status;
+}
+
+enum nss_status 
+_nss_exfiles_getpwbyuid_r(uid_t uid,
+                        struct passwd *pwbuf,
+                        char *buffer,
+                        size_t buflen, 
+                        int *errnop)
+{
+    return _nss_exfiles_getpwuid_r(
+                        uid,
+                        pwbuf,
+                        buffer,
+                        buflen, 
+                        errnop
+                        )
+        ;
+
 }
 
 
@@ -140,18 +155,20 @@ _nss_exfiles_getpwnam_r(const char *pwnam,
 
     pwnam_length = strlen(pwnam);
 
-    _nss_exfiles_setpwent();
-    while (NSS_STATUS_SUCCESS == _nss_exfiles_getpwent_r(pwbuf, NULL, 0, NULL)){
-        /* Compare requested username with current entry 
-         * first checking that they're the same length 
-         */ 
-        if (strlen(pwbuf->pw_name) != pwnam_length)
-            break;
+    if (NSS_STATUS_SUCCESS == _nss_exfiles_setpwent()) {
+        while (NSS_STATUS_SUCCESS == 
+                _nss_exfiles_getpwent_r(pwbuf, buffer, buflen, errnop)){
+            /* Compare requested username with current entry 
+             * first checking that they're the same length 
+             */ 
+            if (strlen(pwbuf->pw_name) != pwnam_length)
+                break;
 
-        /* then comparing by char */
-        if (0 == strncmp(pwnam, pwbuf->pw_name, pwnam_length)) {
-            status = NSS_STATUS_SUCCESS;
-            break;
+            /* then comparing by char */
+            if (0 == strncmp(pwnam, pwbuf->pw_name, pwnam_length)) {
+                status = NSS_STATUS_SUCCESS;
+                break;
+            }
         }
     }
     _nss_exfiles_endpwent();
