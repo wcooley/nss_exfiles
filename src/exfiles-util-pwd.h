db1decb4d5b54cac7c7d522db5c4b67e05b61a0d
Wil Cooley <wcooley@nakedape.cc>
2007-02-06 05:24:25 +0000

    Moved passwd utils to separate module

---
+++ src/exfiles-util-pwd.h
@@ -0,0 +1,19 @@
+#ifndef EXFILES_UTIL_PWD_H
+#define EXFILES_UTIL_PWD_H
+
+/* Password functions */
+
+int exfiles_passwd_alloc_from_pw_entry(struct passwd *, char **);
+
+int exfiles_copy_passwd_from_pw_entry(struct passwd *, char **);
+
+void exfiles_passwd_destroy(struct passwd *);
+
+int print_passwd_struct(FILE *, const struct passwd *);
+
+int pretty_print_passwd_struct(FILE *, const struct passwd *);
+
+int exfiles_passwd_cmp(const struct passwd *, const struct passwd *);
+
+#endif /* EXFILES_UTIL_PWD_H */
+
--- src/exfiles-util.h
+++ src/exfiles-util.h
@@ -6,18 +6,7 @@ int exfiles_open_file(char *, FILE **);
 
 int exfiles_set_close_on_exec(FILE *);
 
-int exfiles_passwd_alloc_from_pw_entry(struct passwd *, char **);
-
-int exfiles_copy_passwd_from_pw_entry(struct passwd *, char **);
-
-void exfiles_passwd_destroy(struct passwd *);
-
-int print_passwd_struct(FILE *, const struct passwd *);
-
-int pretty_print_passwd_struct(FILE *, const struct passwd *);
-
-int exfiles_passwd_cmp(const struct passwd *, const struct passwd *);
-
 void exfiles_trace_msg(char *);
+
 
 #endif /* EXFILES_UTIL_H */
