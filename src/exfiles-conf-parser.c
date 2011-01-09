e449a40fa177ace2b843fb4d773b0a132d6f1969
Wil Cooley <wcooley@nakedape.cc>
2011-01-09 07:12:27 +0100

    Started fleshing out conf parser
    * Use more appriate sizes for scanf buffers and include those limits in
      the format strings.
    
    * Started building config structure.

--- src/exfiles-conf-parser.c
+++ src/exfiles-conf-parser.c
@@ -1,17 +1,25 @@
+#include <limits.h>
 #include <stdio.h>
 #include "strlist.h"
+
+#define MAX_NSS 10 /* "protocols" and "publickey" are both strlen 9 */
+
+#define XSTR(s) STR(s)
+#define STR(s) #s
 
 int
 nss_exfiles_parse_config (FILE *cfg) {
 
-    char type[64], file[64];
+    char type[MAX_NSS], file[PATH_MAX+1];
 
     while (!feof(cfg)) {
 
         if (fscanf(cfg, "# %*[^\n]\n") > 0) /* Comment */
             continue;
 
-        if (fscanf(cfg, " %[a-z]: %s ", type, file) > 0) {
+        if (fscanf(cfg, " %" XSTR(MAX_NSS)  "[a-z]:"
+                        " %" XSTR(PATH_MAX) "s ",
+                            type, file) == 2 ) {
             printf("type = %s, file = %s\n", type, file);
         }
 
@@ -20,9 +28,14 @@ nss_exfiles_parse_config (FILE *cfg) {
     return 0;
 }
 
+/*
+int
+nss_exfiles_init_config(k
+*/
+
 int
 main (void) {
-    _nss_exfiles_parse_config(stdin);
+    nss_exfiles_parse_config(stdin);
 
     return 0;
 }
--- src/exfiles-conf-parser.h
+++ src/exfiles-conf-parser.h
@@ -1,4 +1,9 @@
 #ifndef EXFILES_CONF_PARSER_H
 #define EXFILES_CONF_PARSER_H
 
+struct exfiles_conf {
+    strlist passwd;
+    strlist group;
+}
+
 #endif /* EXFILES_CONF_PARSER_H */
