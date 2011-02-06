b2b4b6fca105a2fd12ca04417796c35af456d457
Wil Cooley <wcooley@nakedape.cc>
2011-02-05 17:32:13 -0800

    Changed & implemented several parser-related items
    * Implemented functions to init the config struct and print it.
    
    * Use fixed-string scans for database type.
    
    * Implemented actually filling out conf struct with config data.
    
    * Got rid of 'nss_' prefix.

--- src/exfiles-conf-parser.c
+++ src/exfiles-conf-parser.c
@@ -1,41 +1,96 @@
 #include <limits.h>
 #include <stdio.h>
+#include <string.h>
 #include "strlist.h"
-
-#define MAX_NSS 10 /* "protocols" and "publickey" are both strlen 9 */
+#include "exfiles-conf-parser.h"
 
 #define XSTR(s) STR(s)
 #define STR(s) #s
 
 int
-nss_exfiles_parse_config (FILE *cfg) {
+exfiles_parse_config(FILE *cfgfile, struct exfiles_conf *conf) {
 
-    char type[MAX_NSS], file[PATH_MAX+1];
+    char file[PATH_MAX+1];
 
-    while (!feof(cfg)) {
+    while (!feof(cfgfile)) {
 
-        if (fscanf(cfg, "# %*[^\n]\n") > 0) /* Comment */
+        file[0] = '\0';
+
+        if (fscanf(cfgfile, "# %*[^\n]\n") > 0) /* Comment */
             continue;
 
-        if (fscanf(cfg, " %" XSTR(MAX_NSS)  "[a-z]:"
-                        " %" XSTR(PATH_MAX) "s ",
-                            type, file) == 2 ) {
-            printf("type = %s, file = %s\n", type, file);
+        if (fscanf(cfgfile, " passwd: %" XSTR(PATH_MAX) "s", file) == 1) {
+            printf("type = passwd, file = %s\n", file);
+            strlist_append_str(conf->passwd, file);
         }
 
-        type[0] = file[0] = '\0';
+        else if (fscanf(cfgfile, " group: %" XSTR(PATH_MAX) "s", file) == 1) {
+            printf("type = group, file = %s\n", file);
+            strlist_append_str(conf->group, file);
+        }
+
+        else {
+            /* Gobble remaining unrecognized junk */
+            fscanf(cfgfile, "%*s");
+        }
     }
+
+    /* exfiles_print_config(conf); */
+
     return 0;
 }
+
+int
+exfiles_init_config(struct exfiles_conf *conf) {
+
+    conf->passwd = strlist_create_list();
+    if (conf->passwd == NULL)
+        return -1;
+
+    conf->group = strlist_create_list();
+    if (conf->group == NULL) {
+        strlist_destroy_list(conf->passwd);
+        return -1;
+    }
+
+    return 0;
+}
+
+void
+exfiles_print_config(struct exfiles_conf *conf) {
+    strlist_node *listnext;
+
+    listnext = conf->passwd->head;
+
+    printf("passwd:");
+    while (listnext != NULL) {
+        printf(" '%s'", listnext->string);
+        listnext = listnext->next;
+    }
+    printf("\n");
+
+    listnext = conf->group->head;
+
+    printf("group:");
+    while (listnext != NULL) {
+        printf(" '%s'", listnext->string);
+        listnext = listnext->next;
+    }
+    printf("\n");
+
+}
+
 
 /*
 int
-nss_exfiles_init_config(k
-*/
-
-int
 main (void) {
-    nss_exfiles_parse_config(stdin);
+
+    struct exfiles_conf conf;
+
+    exfiles_init_config(&conf);
+    exfiles_parse_config(stdin, &conf);
+    exfiles_print_config(&conf);
 
     return 0;
 }
+*/
--- src/exfiles-conf-parser.h
+++ src/exfiles-conf-parser.h
@@ -1,9 +1,17 @@
 #ifndef EXFILES_CONF_PARSER_H
 #define EXFILES_CONF_PARSER_H
 
+#include "strlist.h"
+
 struct exfiles_conf {
-    strlist passwd;
-    strlist group;
-}
+    strlist *passwd;
+    strlist *group;
+};
+
+int exfiles_parse_config(FILE *cfgfile, struct exfiles_conf *c);
+
+int exfiles_init_config(struct exfiles_conf *c);
+
+void exfiles_print_config(struct exfiles_conf *c);
 
 #endif /* EXFILES_CONF_PARSER_H */
