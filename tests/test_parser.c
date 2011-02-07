28359df89d7e652adc38e6434e55b351fee24709
Wil Cooley <wcooley@nakedape.cc>
2011-02-06 21:21:11 -0800

    Implemented tests of reference config file

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -5,6 +5,18 @@
 
 #include "exfiles-conf-parser.h"
 
+#define REFERENCE_CONF "../test-data/exfiles-conf"
+
+/* From a later version of 'check' than is packaged for RHEL5 */
+#ifndef ck_assert_msg
+#define ck_assert_msg fail_unless
+/* String comparsion macros with improved output compared to fail_unless() */
+#define _ck_assert_str(C, X, O, Y) ck_assert_msg(C, \
+        "Assertion '"#X#O#Y"' failed: "#X"==\"%s\", "#Y"==\"%s\"", X, Y)
+#define ck_assert_str_eq(X, Y) _ck_assert_str(!strcmp(X, Y), X, ==, Y)
+#define ck_assert_str_ne(X, Y) _ck_assert_str(strcmp(X, Y), X, !=, Y)
+#endif /* ck_assert_msg */
+
 START_TEST (test_parser_null)
 {
     FILE *cfgfile;
@@ -28,15 +40,114 @@ START_TEST (test_parser_null)
 }
 END_TEST
 
+
+struct exfiles_conf *reference_conf;
+
+#define addpasswd(PFILE) (strlist_append_str(reference_conf->passwd, PFILE))
+#define addgroup(GFILE)  (strlist_append_str(reference_conf->group, GFILE))
+
+void setup_reference_conf(void) {
+
+    reference_conf = calloc(1, sizeof(struct exfiles_conf));
+
+    reference_conf->passwd = strlist_create_list();
+    reference_conf->group  = strlist_create_list();
+
+    addpasswd("/var/foo-pass");
+    addpasswd("/etc/bar-pass");
+    addpasswd("/bar/baz");
+    addpasswd("sdlfs");
+    addpasswd("/alt/binaries/pictures");
+
+    addgroup("/tmp/group");
+    addgroup("/nfs/groupie");
+    addgroup("/bab/zab");
+    addgroup("/tab/over-here");
+}
+
+void teardown_reference_conf(void) {
+    /* FIXME */
+}
+
+void compare_lists(const char *type, strlist_node *strptr,
+                    strlist_node *refstrptr) {
+
+    char parsed_ran_out[64], ref_ran_out[64];
+
+    sprintf(parsed_ran_out,
+            "Parsed %s data ran out before reference", type );
+
+    sprintf(ref_ran_out,
+            "Reference %s data ran out before parsed", type );
+
+    while( strptr != NULL || refstrptr != NULL ) {
+        fail_if(strptr == NULL, parsed_ran_out);
+        fail_if(refstrptr == NULL, ref_ran_out);
+        ck_assert_str_eq( strptr->string, refstrptr->string );
+        strptr = strptr->next;
+        refstrptr = refstrptr->next;
+    }
+}
+
+START_TEST(test_reference_conf_passwd)
+{
+    FILE *cfgfile;
+    struct exfiles_conf conf;
+    strlist_node *strptr, *refstrptr;
+
+    cfgfile = fopen(REFERENCE_CONF, "r");
+
+    fail_unless( exfiles_init_config(&conf) == 0,
+                "error initializing exfiles_conf struct");
+
+    fail_unless( exfiles_parse_config(cfgfile, &conf) == 0,
+                "error parsing config file");
+
+    strptr = (conf.passwd)->head;
+    refstrptr = (reference_conf->passwd)->head;
+
+    compare_lists("passwd", strptr, refstrptr);
+}
+END_TEST
+
+START_TEST(test_reference_conf_group)
+{
+    FILE *cfgfile;
+    struct exfiles_conf conf;
+    strlist_node *strptr, *refstrptr;
+
+    cfgfile = fopen(REFERENCE_CONF, "r");
+
+    fail_unless( exfiles_init_config(&conf) == 0,
+                "error initializing exfiles_conf struct");
+
+    fail_unless( exfiles_parse_config(cfgfile, &conf) == 0,
+                "error parsing config file");
+
+    strptr = (conf.group)->head;
+    refstrptr = (reference_conf->group)->head;
+
+    compare_lists("group", strptr, refstrptr);
+}
+END_TEST
+
 Suite *
 parser_suite(void)
 {
     Suite *tsuite = suite_create("parser");
-    TCase *tc_core = tcase_create("Core");
+    TCase *tc_null = tcase_create("Null");
+    TCase *tc_ref = tcase_create("Reference");
 
-    suite_add_tcase(tsuite, tc_core);
+    /* Null Conf */
+    suite_add_tcase(tsuite, tc_null);
+    tcase_add_test(tc_null, test_parser_null);
 
-    tcase_add_test(tc_core, test_parser_null);
+    /* Reference Conf */
+    suite_add_tcase(tsuite, tc_ref);
+    tcase_add_checked_fixture(tc_ref, setup_reference_conf,
+                                teardown_reference_conf);
+    tcase_add_test(tc_ref, test_reference_conf_passwd);
+    tcase_add_test(tc_ref, test_reference_conf_group);
 
     return tsuite;
 }
